adfc3af1d04b8fba37d9a9c8588997183ea24670
muhammed <ben@muhammed.im>
2020-10-27 22:00:41 +0300

    init project

---
+++ android/app/src/main/kotlin/im/muhammed/yababos/MainActivity.kt
@@ -0,0 +1,6 @@
+package im.muhammed.yababos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Yababos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yababos',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
+}
