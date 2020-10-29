38651df4ddef38b03091f48e9edf331e7db17980
muhammed <ben@muhammed.im>
2020-10-29 23:12:56 +0300

    transaction bloc

---
+++ lib/blocs/transaction.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yababos/events/transaction.dart';
+import 'package:yababos/models/transaction_repository.dart';
+import 'package:yababos/states/transaction.dart';
+
+class TransactionBloc extends Bloc<TransactionEvent, TransactionState> {
+  final TransactionRepository db;
+
+  TransactionBloc({@required this.db}) : super(TransactionLoading());
+
+  @override
+  Stream<TransactionState> mapEventToState(TransactionEvent event) async* {
+    if (event is TransactionAdd) {
+      yield await _mapAddtoState(event);
+    } else if (event is TransactionDelete) {
+      yield await _mapDeletetoState(event);
+    } else if (event is TransactionUpdate) {
+      yield await _mapUpdatetoState(event);
+    } else if (event is TransactionGet) {
+      yield await _mapGettoState(event);
+    }
+  }
+
+  Future<TransactionState> _mapAddtoState(TransactionAdd event) async {
+    db.add(event.transaction);
+    return TransactionLoaded();
+  }
+
+  Future<TransactionState> _mapDeletetoState(TransactionDelete event) async {
+    db.delete(event.id);
+    return TransactionLoaded();
+  }
+
+  Future<TransactionState> _mapUpdatetoState(TransactionUpdate event) async {
+    db.update(event.transaction);
+    return TransactionLoaded();
+  }
+
+  Future<TransactionState> _mapGettoState(TransactionGet event) async {
+    return TransactionLoaded.one(await db.get(event.id));
+  }
+}
