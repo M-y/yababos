c2664b2401b1e96f79910e3822c50bc3aa086cbb
muhammed <ben@muhammed.im>
2020-10-29 23:11:26 +0300

    TransactionLoaded state one transaction constructor

--- lib/states/transaction.dart
+++ lib/states/transaction.dart
@@ -1,7 +1,14 @@
+import 'package:yababos/models/transaction.dart';
+
 abstract class TransactionState {
   const TransactionState();
 }
 
 class TransactionLoading extends TransactionState {}
 
-class TransactionLoaded extends TransactionState {}
+class TransactionLoaded extends TransactionState {
+  Transaction transaction;
+
+  TransactionLoaded();
+  TransactionLoaded.one(this.transaction);
+}
