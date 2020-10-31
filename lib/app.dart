fe618fc6b94dde27376baa525b4a6eda4d52e135
muhammed <ben@muhammed.im>
2020-10-31 17:25:29 +0300

    TransactionBloc and l10n implementation

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'generated/l10n.dart';
+import 'package:yababos/blocs/transaction.dart';
+import 'package:yababos/models/inmemory/transaction.dart';
 import 'package:yababos/views/wallet.dart';
 
 class Yababos extends StatelessWidget {
@@ -6,10 +11,19 @@ class Yababos extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Yababos',
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Wallet(),
+      home: BlocProvider(
+        create: (context) => TransactionBloc(db: TransactionInmemory()),
+        child: Wallet(),
+      ),
     );
   }
 }
--- lib/views/wallet.dart
+++ lib/views/wallet.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:yababos/generated/l10n.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yababos/blocs/transaction.dart';
+import 'package:yababos/events/transaction.dart';
+import 'package:yababos/models/transaction.dart';
+import 'package:yababos/states/transaction.dart';
 
 class Wallet extends StatefulWidget {
   @override
@@ -8,15 +15,38 @@ class Wallet extends StatefulWidget {
 class WalletState extends State<Wallet> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Wallet'),
-      ),
-      body: ListView(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
-      ),
+    return BlocBuilder<TransactionBloc, TransactionState>(
+      builder: (context, state) {
+        final Transaction transaction1 =
+            Transaction(id: 1, from: null, to: null);
+        final Transaction transaction2 =
+            Transaction(id: 2, from: null, to: null);
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(S.of(context).wallet),
+          ),
+          body: ListView(),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              BlocProvider.of<TransactionBloc>(context)
+                  .add(TransactionAdd(transaction1));
+
+              transaction1.description = 'test';
+              BlocProvider.of<TransactionBloc>(context)
+                  .add(TransactionUpdate(transaction1));
+
+              BlocProvider.of<TransactionBloc>(context)
+                  .add(TransactionDelete(transaction1.id));
+              BlocProvider.of<TransactionBloc>(context)
+                  .add(TransactionAdd(transaction2));
+              BlocProvider.of<TransactionBloc>(context)
+                  .add(TransactionGet(transaction2.id));
+            },
+          ),
+        );
+      },
     );
   }
 }
