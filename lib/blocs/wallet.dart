5bca8d940067ddc22195f1f8346be3ea33549f74
Muhammed YILDIRIM <ben@muhammed.im>
2020-12-13 16:40:34 +0300

    get selected wallet from SettingsBloc

--- lib/blocs/wallet.dart
+++ lib/blocs/wallet.dart
@@ -1,12 +1,25 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yababos/blocs/settings.dart';
 import 'package:yababos/events/wallet.dart';
+import 'package:yababos/models/wallet.dart';
 import 'package:yababos/models/wallet_repository.dart';
+import 'package:yababos/states/settings.dart';
 import 'package:yababos/states/wallet.dart';
 
 class WalletBloc extends Bloc<WalletEvent, WalletState> {
   final WalletRepository _walletRepository;
+  final SettingsBloc _settingsBloc;
+  Wallet _selectedWallet;
 
-  WalletBloc(this._walletRepository) : super(WalletLoading());
+  WalletBloc(this._walletRepository, this._settingsBloc)
+      : super(WalletLoading()) {
+    _settingsBloc.listen((state) async {
+      if (state is SettingChanged && state.setting.name == 'wallet') {
+        _selectedWallet = await _walletRepository.get(state.setting.value);
+        this.add(WalletGetAll());
+      }
+    });
+  }
 
   @override
   Stream<WalletState> mapEventToState(WalletEvent event) async* {
@@ -24,18 +37,27 @@ class WalletBloc extends Bloc<WalletEvent, WalletState> {
   }
 
   Future<WalletState> _mapAddtoState(WalletAdd event) async {
-    _walletRepository.add(event.wallet);
-    return WalletLoaded(await _walletRepository.getAll());
+    await _walletRepository.add(event.wallet);
+    return WalletLoaded(
+      wallets: await _walletRepository.getAll(),
+      selectedWallet: _selectedWallet,
+    );
   }
 
   Future<WalletState> _mapDeletetoState(WalletDelete event) async {
-    _walletRepository.delete(event.id);
-    return WalletLoaded(await _walletRepository.getAll());
+    await _walletRepository.delete(event.id);
+    return WalletLoaded(
+      wallets: await _walletRepository.getAll(),
+      selectedWallet: _selectedWallet,
+    );
   }
 
   Future<WalletState> _mapUpdatetoState(WalletUpdate event) async {
-    _walletRepository.update(event.wallet);
-    return WalletLoaded(await _walletRepository.getAll());
+    await _walletRepository.update(event.wallet);
+    return WalletLoaded(
+      wallets: await _walletRepository.getAll(),
+      selectedWallet: _selectedWallet,
+    );
   }
 
   Future<WalletState> _mapGettoState(WalletGet event) async {
@@ -43,6 +65,9 @@ class WalletBloc extends Bloc<WalletEvent, WalletState> {
   }
 
   _mapGetAlltoState(WalletGetAll event) async {
-    return WalletLoaded(await _walletRepository.getAll());
+    return WalletLoaded(
+      wallets: await _walletRepository.getAll(),
+      selectedWallet: _selectedWallet,
+    );
   }
 }
