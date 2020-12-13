0351063060887701602e1770961d1d019adbc4ac
Muhammed YILDIRIM <ben@muhammed.im>
2020-12-13 16:39:14 +0300

    selected wallet property for walletstate

--- lib/states/wallet.dart
+++ lib/states/wallet.dart
@@ -8,8 +8,12 @@ class WalletLoading extends WalletState {}
 
 class WalletLoaded extends WalletState {
   Wallet wallet;
+  Wallet selectedWallet;
   List<Wallet> wallets;
 
-  WalletLoaded(this.wallets);
+  WalletLoaded({this.wallets, this.selectedWallet}) {
+    if (wallets.length > 0 && selectedWallet == null)
+      selectedWallet = wallets[wallets.length - 1];
+  }
   WalletLoaded.one(this.wallet);
 }
