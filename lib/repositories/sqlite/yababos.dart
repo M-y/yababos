6a600b41e85c0ca1d06ee56d80ec9c730b523082
Muhammed YILDIRIM <ben@muhammed.im>
2021-10-25 22:09:34 +0300

    sqlite repository setup

---
+++ lib/repositories/sqlite/yababos.dart
@@ -0,0 +1,56 @@
+import 'package:sqflite/sqflite.dart';
+
+class YababosSqlite {
+  static String path;
+
+  static final int version = 1;
+
+  static final String onCreate = '''
+CREATE TABLE "transactions" (
+	"id"	INTEGER,
+	"fromWallet"	INTEGER,
+	"toWallet"	INTEGER,
+	"amount"	REAL,
+	"date"	INTEGER,
+	"description"	TEXT,
+	PRIMARY KEY("id")
+);
+CREATE TABLE "tags" (
+	"name"	TEXT,
+	"color"	INTEGER NOT NULL,
+	PRIMARY KEY("name")
+);
+CREATE TABLE "transaction_tags" (
+	"transaction"	INTEGER,
+	"tag"	TEXT,
+	FOREIGN KEY("transaction") REFERENCES "transactions"("id"),
+	FOREIGN KEY("tag") REFERENCES "tags"("name")
+);
+CREATE TABLE "wallet" (
+	"id"	INTEGER,
+	"name"	TEXT NOT NULL,
+	"curreny"	TEXT NOT NULL,
+	"amount"	REAL NOT NULL DEFAULT 0,
+	PRIMARY KEY("id")
+);
+CREATE TABLE "settings" (
+	"name"	TEXT,
+	"value"	BLOB,
+	PRIMARY KEY("name")
+);
+  ''';
+
+  static final String onUpgrade = '''
+
+  ''';
+
+  static Future<Database> getDatabase() async {
+    return openDatabase(
+      path,
+      version: version,
+      onCreate: (db, version) => db.execute(onCreate),
+      onUpgrade: (db, oldVersion, newVersion) => db.execute(onUpgrade),
+      singleInstance: true,
+    );
+  }
+}
