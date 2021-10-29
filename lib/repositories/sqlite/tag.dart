13db7f1b33a6fd6b8facd58e8f4c33e97b8cbace
Muhammed YILDIRIM <ben@muhammed.im>
2021-10-29 18:08:30 +0300

    sqlite tag repository

---
+++ lib/repositories/sqlite/tag.dart
@@ -0,0 +1,98 @@
+import 'dart:ui';
+import 'package:yababos/models/tag.dart';
+import 'package:yababos/repositories/sqlite/yababos.dart';
+import 'package:yababos/repositories/tag_repository.dart';
+
+class TagSqlite extends TagRepository {
+  @override
+  Future add(Tag tag) {
+    return Future(() async {
+      await (await YababosSqlite.getDatabase()).rawInsert('''
+        INSERT INTO tags
+        (
+          name,
+          color
+        )
+        VALUES
+        (
+          ?,
+          ?
+        )
+        ''', [
+        tag.name,
+        tag.color.value,
+      ]);
+    });
+  }
+
+  @override
+  Future delete(String name) {
+    return Future(() async {
+      await (await YababosSqlite.getDatabase())
+          .rawDelete('DELETE FROM tags WHERE name = ?', [name]);
+    });
+  }
+
+  @override
+  Future<List<Tag>> find(Tag tag) {
+    return Future(() async {
+      List<Tag> tags = List<Tag>();
+      List<Map<String, Object>> records =
+          await (await YababosSqlite.getDatabase()).rawQuery(
+              'SELECT * FROM tags WHERE name LIKE ?', ["%${tag.name}%"]);
+      for (var record in records) {
+        tags.add(_mapRecord(record));
+      }
+      return tags;
+    });
+  }
+
+  @override
+  Future<Tag> get(String name) {
+    return Future(() async {
+      List<Map<String, Object>> record =
+          await (await YababosSqlite.getDatabase())
+              .rawQuery('SELECT * FROM tags WHERE name = ?', [name]);
+      if (record.isEmpty) return null;
+      return _mapRecord(record[0]);
+    });
+  }
+
+  @override
+  Future<List<Tag>> getAll() {
+    return Future(() async {
+      List<Tag> tags = List<Tag>();
+      List<Map<String, Object>> records =
+          await (await YababosSqlite.getDatabase())
+              .rawQuery('SELECT * FROM tags');
+      for (var record in records) {
+        tags.add(_mapRecord(record));
+      }
+      return tags;
+    });
+  }
+
+  @override
+  Future update(String oldName, Tag tag) {
+    return Future(() async {
+      if (await get(oldName) == null) throw new Exception();
+      await (await YababosSqlite.getDatabase()).rawUpdate('''
+        UPDATE tags SET
+          name = ?,
+          color = ?
+        WHERE name = ?
+        ''', [
+        tag.name,
+        tag.color.value,
+        oldName,
+      ]);
+    });
+  }
+
+  Tag _mapRecord(Map<String, Object> record) {
+    return Tag(
+      name: record['name'],
+      color: Color(record['color']),
+    );
+  }
+}
--- test/repository_test.dart
+++ test/repository_test.dart
@@ -6,6 +6,7 @@ import 'package:yababos/repositories/inmemory/wallet.dart';
 import 'package:yababos/models/setting.dart';
 import 'package:yababos/repositories/settings_repository.dart';
 import 'package:yababos/models/tag.dart';
+import 'package:yababos/repositories/sqlite/tag.dart';
 import 'package:yababos/repositories/sqlite/transaction.dart';
 import 'package:yababos/repositories/sqlite/yababos.dart';
 import 'package:yababos/repositories/tag_repository.dart';
@@ -46,48 +47,53 @@ void main() {
   });
 
   group('Tag', () {
-    TagRepository tagRepository = TagInmemory();
+    List<TagRepository> repositories = List.from([
+      TagInmemory(),
+      TagSqlite(),
+    ]);
 
-    test('add', () async {
-      Tag sampleTag = Tag(name: 'test');
-      await tagRepository.add(sampleTag);
+    for (TagRepository tagRepository in repositories) {
+      test('add', () async {
+        Tag sampleTag = Tag(name: 'test');
+        await tagRepository.add(sampleTag);
 
-      expect(await tagRepository.get(sampleTag.name), sampleTag);
-    });
+        expect(await tagRepository.get(sampleTag.name), sampleTag);
+      });
 
-    test('get', () async {
-      expect((await tagRepository.get('test')).name, 'test');
-      expect(await tagRepository.get('not available'), null);
-    });
+      test('get', () async {
+        expect((await tagRepository.get('test')).name, 'test');
+        expect(await tagRepository.get('not available'), null);
+      });
 
-    test('getAll', () async {
-      expect(await tagRepository.getAll(), isInstanceOf<List<Tag>>());
-    });
+      test('getAll', () async {
+        expect(await tagRepository.getAll(), isInstanceOf<List<Tag>>());
+      });
 
-    test('update', () async {
-      Tag updateTag = Tag(name: 'updated');
-      await tagRepository.update('test', updateTag);
+      test('update', () async {
+        Tag updateTag = Tag(name: 'updated');
+        await tagRepository.update('test', updateTag);
 
-      expect(await tagRepository.get('test'), null);
-      expect(tagRepository.update('test', updateTag), throwsException);
-      expect(await tagRepository.get('updated'), updateTag);
-    });
+        expect(await tagRepository.get('test'), null);
+        expect(tagRepository.update('test', updateTag), throwsException);
+        expect(await tagRepository.get('updated'), updateTag);
+      });
 
-    test('find', () async {
-      Tag find = Tag(name: 'updated');
-      Tag toNotFind = Tag(name: 'not available');
+      test('find', () async {
+        Tag find = Tag(name: 'up');
+        Tag toNotFind = Tag(name: 'not available');
 
-      expect(await tagRepository.find(find), isInstanceOf<List<Tag>>());
-      expect((await tagRepository.find(find)).length, 1);
-      expect(await tagRepository.find(toNotFind), isInstanceOf<List<Tag>>());
-      expect((await tagRepository.find(toNotFind)).length, 0);
-    });
+        expect(await tagRepository.find(find), isInstanceOf<List<Tag>>());
+        expect((await tagRepository.find(find)).length, 1);
+        expect(await tagRepository.find(toNotFind), isInstanceOf<List<Tag>>());
+        expect((await tagRepository.find(toNotFind)).length, 0);
+      });
 
-    test('delete', () async {
-      await tagRepository.delete('updated');
+      test('delete', () async {
+        await tagRepository.delete('updated');
 
-      expect(await tagRepository.get('updated'), null);
-    });
+        expect(await tagRepository.get('updated'), null);
+      });
+    }
   });
 
   group('Transaction', () {
