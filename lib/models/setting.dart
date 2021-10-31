c51496d9f73c64c7a990cb60a5a15014cf91f1c8
Muhammed YILDIRIM <ben@muhammed.im>
2021-10-31 14:05:38 +0300

    make setting model equatable

--- lib/models/setting.dart
+++ lib/models/setting.dart
@@ -1,8 +1,12 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 
-class Setting {
+class Setting extends Equatable {
   String name;
   Object value;
 
   Setting({@required this.name, this.value});
+
+  @override
+  List<Object> get props => [name, value];
 }
