6b9232c4f23d69fd80de8da3d0140db09765f708
Muhammed YILDIRIM <ben@muhammed.im>
2022-08-14 12:02:36 +0300

    l10n generated code

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -7,7 +7,8 @@
 // ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -19,27 +20,29 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
-  final Map<String, dynamic> messages = _notInlinedMessages(_notInlinedMessages);
-  static _notInlinedMessages(_) => <String, Function> {
-    "amount" : MessageLookupByLibrary.simpleMessage("Amount"),
-    "backup" : MessageLookupByLibrary.simpleMessage("Backup"),
-    "currency" : MessageLookupByLibrary.simpleMessage("Currency"),
-    "delete" : MessageLookupByLibrary.simpleMessage("Delete"),
-    "description" : MessageLookupByLibrary.simpleMessage("Description"),
-    "editTag" : MessageLookupByLibrary.simpleMessage("Edit Tag"),
-    "editTransaction" : MessageLookupByLibrary.simpleMessage("Edit Transaction"),
-    "editWallet" : MessageLookupByLibrary.simpleMessage("Edit Wallet"),
-    "from" : MessageLookupByLibrary.simpleMessage("From"),
-    "initialAmount" : MessageLookupByLibrary.simpleMessage("Initial Amount"),
-    "name" : MessageLookupByLibrary.simpleMessage("Name"),
-    "newTag" : MessageLookupByLibrary.simpleMessage("New Tag"),
-    "newTransaction" : MessageLookupByLibrary.simpleMessage("New Transaction"),
-    "newWallet" : MessageLookupByLibrary.simpleMessage("New Wallet"),
-    "noTags" : MessageLookupByLibrary.simpleMessage("No tags"),
-    "noWallets" : MessageLookupByLibrary.simpleMessage("No wallets"),
-    "outside" : MessageLookupByLibrary.simpleMessage("Outside"),
-    "tags" : MessageLookupByLibrary.simpleMessage("Tags"),
-    "wallet" : MessageLookupByLibrary.simpleMessage("Wallet"),
-    "wallets" : MessageLookupByLibrary.simpleMessage("Wallets")
-  };
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "amount": MessageLookupByLibrary.simpleMessage("Amount"),
+        "backup": MessageLookupByLibrary.simpleMessage("Backup"),
+        "currency": MessageLookupByLibrary.simpleMessage("Currency"),
+        "delete": MessageLookupByLibrary.simpleMessage("Delete"),
+        "description": MessageLookupByLibrary.simpleMessage("Description"),
+        "editTag": MessageLookupByLibrary.simpleMessage("Edit Tag"),
+        "editTransaction":
+            MessageLookupByLibrary.simpleMessage("Edit Transaction"),
+        "editWallet": MessageLookupByLibrary.simpleMessage("Edit Wallet"),
+        "from": MessageLookupByLibrary.simpleMessage("From"),
+        "initialAmount": MessageLookupByLibrary.simpleMessage("Initial Amount"),
+        "name": MessageLookupByLibrary.simpleMessage("Name"),
+        "newTag": MessageLookupByLibrary.simpleMessage("New Tag"),
+        "newTransaction":
+            MessageLookupByLibrary.simpleMessage("New Transaction"),
+        "newWallet": MessageLookupByLibrary.simpleMessage("New Wallet"),
+        "noTags": MessageLookupByLibrary.simpleMessage("No tags"),
+        "noWallets": MessageLookupByLibrary.simpleMessage("No wallets"),
+        "outside": MessageLookupByLibrary.simpleMessage("Outside"),
+        "tags": MessageLookupByLibrary.simpleMessage("Tags"),
+        "wallet": MessageLookupByLibrary.simpleMessage("Wallet"),
+        "wallets": MessageLookupByLibrary.simpleMessage("Wallets")
+      };
 }
