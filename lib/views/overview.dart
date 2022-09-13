53cfe9a5ffff488dd86e9529113059000db4fda6
M-y <ben.muhammed.im@gmail.com>
2022-09-13 14:10:17 +0000

    income and expenses added to overview widget

--- lib/views/overview.dart
+++ lib/views/overview.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 
 class OverviewWidget extends StatelessWidget {
   final String balance;
+  final String income;
+  final String expense;
 
-  OverviewWidget(this.balance);
+  const OverviewWidget(
+      {required this.balance, required this.income, required this.expense});
 
   @override
   Widget build(BuildContext context) {
-    return Text(balance, style: Theme.of(context).textTheme.headline6);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(balance, style: Theme.of(context).textTheme.headlineLarge),
+        Icon(Icons.arrow_downward, color: Colors.green),
+        Text('+' + income, style: Theme.of(context).textTheme.bodyMedium),
+        Icon(Icons.arrow_upward, color: Colors.red),
+        Text('-' + expense, style: Theme.of(context).textTheme.bodyMedium),
+      ],
+    );
   }
 }
