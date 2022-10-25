62a1c52a0c3ddea50ce8ef3ffe66249871756086
M-y <ben.muhammed.im@gmail.com>
2022-10-25 13:00:22 +0000

    Pie chart for tags

---
+++ lib/views/charts/pie.dart
@@ -0,0 +1,43 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:yababos/models/tag.dart';
+
+class TagChart extends StatefulWidget {
+  final Map<Tag, double> chartData;
+  const TagChart({super.key, required this.chartData});
+
+  @override
+  State<StatefulWidget> createState() => TagChartState();
+}
+
+class TagChartState extends State<TagChart> {
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.8,
+      child: Card(
+        child: Expanded(
+          child: PieChart(
+            PieChartData(
+              centerSpaceRadius: 40,
+              sections: getSections(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<PieChartSectionData> getSections() {
+    List<PieChartSectionData> sectionDatas = List.empty(growable: true);
+    widget.chartData.forEach((key, value) {
+      sectionDatas.add(PieChartSectionData(
+        color: key.color.withOpacity(.9),
+        borderSide: BorderSide(color: key.color, width: 1.5),
+        value: value,
+        title: key.name,
+      ));
+    });
+    return sectionDatas;
+  }
+}
